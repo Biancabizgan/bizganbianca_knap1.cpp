1c96b883ebaea4a55691f991ae470ef7452843c0
Biancabizgan <35563665+Biancabizgan@users.noreply.github.com>
2019-04-02 17:45:48 +0300

    Create bizganbianca_knap1.cpp

---
+++ bizganbianca_knap1.cpp
@@ -0,0 +1,23 @@
+#include<fstream>
+using namespace std;
+int S;
+int k;
+int V[100];
+int read_data()
+{
+    
+}
+int sort_data()
+{
+    
+}
+int greedy()
+{
+    
+}
+int main()
+{
+read_data();
+sort_data();
+cout<<" utilizez "<<greedy()<<" bancnote ";    
+}
